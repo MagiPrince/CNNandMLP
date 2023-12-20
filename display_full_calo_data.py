4ab66b52d0e9663f247812132e7179ff26b82baf
MagiPrince <david.nogueiras@outlook.com>
2023-12-20 17:22:04 +0100

    adding new script for plotting cells of calo

---
+++ display_full_calo_data.py
@@ -0,0 +1,44 @@
+import h5py
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
+import numpy as np
+
+# calocell_file = "RealData/calocellD3PD_2018Data.h5"
+# jet_file = "RealData/jetD3PD_2018Data.h5"
+
+calocell_file = "data/calocell/user.cantel.33075755._000001.calocellD3PD_mc16_JZW4.r14423.h5"
+jet_file = "data/jet/user.cantel.33075755._000001.jetD3PD_mc16_JZW4.r14423.h5"
+
+########################################################################
+# Calorimeter
+########################################################################
+
+f = h5py.File(calocell_file, "r")
+
+dset = f["caloCells"]
+
+# Creating a dictionnary of type parameter : index
+data = dset["2d"]
+array_indexes_to_convert = list(data.dtype.fields.keys())
+dict_data_calo = {array_indexes_to_convert[i]: i for i in range(len(array_indexes_to_convert))}
+
+data_calo = dset["2d"][()]
+
+f.close()
+
+df_calo = pd.DataFrame(data_calo[0],  columns=list(dict_data_calo.keys()))
+
+########################################################################
+# Plot
+########################################################################
+
+for i in range(0, len(data_calo)):
+    # Creation of Dataframes pandas
+    df_calo = pd.DataFrame(data_calo[i], columns=list(dict_data_calo.keys()))
+    df_calo = df_calo[df_calo['cell_eta'].abs() < 2.4]
+    
+    plt.plot(df_calo['cell_eta'], df_calo['cell_phi'], linewidth=1, alpha=1)
+    plt.xlabel("eta")
+    plt.ylabel("phi")
+    plt.show()
